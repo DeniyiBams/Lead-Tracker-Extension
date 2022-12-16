87801b0656970c4989b97c3807c2daa6132f32f8
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-12-16 23:11:17 +0100

    Printing the leads in the ul tag

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let inputBtn = document.getElementById("input-btn")
-const myLeads = []
+let myLeads = ["www.google.com", "www.helo.com"]
+const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    let text = inputEl.value
-    myLeads.push(text)
+    myLeads.push(inputEl.value)
 })
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
