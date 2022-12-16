584193dfce0dc52e15677645bf9f2f670c79bb73
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-12-17 00:45:42 +0100

    fix: renderLeads()

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href=https://${myLeads[i]}>${myLeads[i]}</a>
+                <a target='_blank' href='https://${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
             </li>`
     }
     ulEl.innerHTML = listItems
