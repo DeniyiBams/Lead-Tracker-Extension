eceba5b6553f4cef260a5a02ea6a8ed302d2828a
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2023-01-02 21:54:23 +0100

    init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let myLeads = []
+let myLeads = new Object()
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const tabBtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
+const titleEl = document.getElementById("title-el")
+const tableEl = document.getElementById("table-el")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
@@ -13,35 +14,46 @@ if (leadsFromLocalStorage) {
 }
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    myLeads[titleEl.value] = inputEl.value
     inputEl.value=""
+    titleEl.value=""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
 tabBtn.addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLeads()
+        // myLeads.push(tabs[0].url)
+        const tabLink = tabs[0].url
+        inputEl.value = tabLink
+        // localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        // renderLeads()
     })
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
-    myLeads = []
+    myLeads = new Object()
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    
+    for (const key in myLeads) {
         listItems += `
-            <li>
-                <a target='_blank' href='https://${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>`
+        <tbody>
+            <tr>
+                <td>
+                    ${key}
+                </td>
+                <td>
+                    <a target='_blank' href='https://${myLeads[key]}'>
+                        ${myLeads[key]}
+                    </a>
+                </td>
+            </tr>
+        </tbody`
     }
-    ulEl.innerHTML = listItems
+    tableEl.innerHTML += listItems
 }
